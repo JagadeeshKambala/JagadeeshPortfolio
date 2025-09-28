5ff21481983cd0f864f99dcaee633057d286b370
Jagadeesh Kambala <kambala.jagadeesh19@gmail.com>
2025-09-28 09:08:00 +0530

    feat(ui): update certification details in certifications section

--- src/data/certificationsData.ts
+++ src/data/certificationsData.ts
@@ -17,86 +17,66 @@ export interface Company {
 
 export const companies: Company[] = [
   {
-    id: 1,
-    name: 'Google',
-    logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg',
-    certifications: [
-      {
-        id: 1,
-        name: 'Google Cloud Professional Data Engineer',
-        organization: 'Google',
-        issuedDate: 'June 2023',
-        credentialUrl: 'https://example.com/credential',
-        orgLogo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg',
-      },
-      {
-        id: 2,
-        name: 'Google Professional UX Designer',
-        organization: 'Google',
-        issuedDate: 'March 2023',
-        credentialUrl: 'https://example.com/credential',
-        orgLogo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg',
-      },
-    ],
-  },
-  {
     id: 2,
-    name: 'Microsoft',
-    logo: 'src/Microsoft.svg',
+    name: "Microsoft",
+    logo: "src/Microsoft.svg",
     certifications: [
       {
         id: 3,
-        name: 'Introduction to Computers and Operating Systems and Security',
-        organization: 'Microsoft',
-        issuedDate: 'June 2025',
-        credentialUrl: 'https://www.coursera.org/account/accomplishments/certificate/PJTOG4E6RCHJ',
-        orgLogo: 'src/Microsoft.svg',
+        name: "Introduction to Computers and Operating Systems and Security",
+        organization: "Microsoft",
+        issuedDate: "June 2025",
+        credentialUrl:
+          "https://www.coursera.org/account/accomplishments/certificate/PJTOG4E6RCHJ",
+        orgLogo: "src/Microsoft.svg",
       },
     ],
   },
   {
     id: 3,
-    name: 'Meta',
-    logo: 'src/Meta.svg',
+    name: "Meta",
+    logo: "src/Meta.svg",
     certifications: [
       {
         id: 4,
-        name: 'Meta Front-End Developer Professional Certificate',
-        organization: 'Meta',
-        issuedDate: 'December 2024',
-        credentialUrl: 'https://www.coursera.org/account/accomplishments/specialization/certificate/7TKVA1W3JTCN',
-        orgLogo: 'src/Meta.svg',
+        name: "Meta Front-End Developer Professional Certificate",
+        organization: "Meta",
+        issuedDate: "December 2024",
+        credentialUrl:
+          "https://www.coursera.org/account/accomplishments/specialization/certificate/7TKVA1W3JTCN",
+        orgLogo: "src/Meta.svg",
       },
-      
     ],
   },
   {
     id: 5,
-    name: 'Amazon',
-    logo: 'src/aws-svg-logo.png',
+    name: "Amazon",
+    logo: "src/aws-svg-logo.png",
     certifications: [
       {
         id: 6,
-        name: 'AWS Certified Solutions Architect – Associate',
-        organization: 'Amazon',
-        issuedDate: 'May 2025',
-        credentialUrl: 'https://www.coursera.org/account/accomplishments/certificate/YY1VWKP5TM2M',
-        orgLogo: 'src/aws-svg-logo.png',
+        name: "AWS Certified Solutions Architect – Associate",
+        organization: "Amazon",
+        issuedDate: "May 2025",
+        credentialUrl:
+          "https://www.coursera.org/account/accomplishments/certificate/YY1VWKP5TM2M",
+        orgLogo: "src/aws-svg-logo.png",
       },
     ],
   },
   {
     id: 4,
-    name: 'IBM',
-    logo: 'src/Ibm-Logo.png',
+    name: "IBM",
+    logo: "src/Ibm-Logo.png",
     certifications: [
       {
         id: 5,
-        name: 'IBM Data Science Professional Certificate',
-        organization: 'IBM',
-        issuedDate: 'August 2022',
-        credentialUrl: 'https://example.com/credential',
-        orgLogo: 'src/Ibm-Logo.png',
+        name: "Hands-on Introduction to Linux Commands and Shell Scripting",
+        organization: "IBM",
+        issuedDate: "June 2025",
+        credentialUrl:
+          "https://coursera.org/share/0b6060a4f529bef98e79a377b5b7174a",
+        orgLogo: "src/Ibm-Logo.png",
       },
     ],
   },
